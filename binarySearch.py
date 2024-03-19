61b46a673bca4d2b65026ccd96db413d732ed606
W. Machado <104407345+winissius@users.noreply.github.com>
2024-03-18 22:54:50 -0300

    Binary search test

--- binarySearch.py
+++ binarySearch.py
@@ -2,7 +2,7 @@ def binarySearch(list, item):
     lower = 0
     higher = len(list) - 1
     while lower <= higher:
-        middle = (lower + higher) / 2 # round down
+        middle = (lower + higher) // 2  # round down
         guess = list[middle]
         if guess == item:
             return middle
@@ -10,10 +10,11 @@ def binarySearch(list, item):
             higher = middle - 1
         else:
             lower = middle + 1
+    return None
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(len(list) - 1)
-lower = 0
-higher = len(list) - 1
-middle = (lower + higher) // 2
-print(middle)
+
+myList = [1, 3, 5, 7, 9]
+print(binarySearch(myList, 3))
+
+myList = [1, 3, 5, 7, 9]
+print(binarySearch(myList, -1))
